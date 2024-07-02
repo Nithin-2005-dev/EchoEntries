ce6db9ee3fcccfdc66b3e83f705c7ef41be7bc0c
Nithin-2005-dev <bingu_ug_23@ee.nits.ac.in>
2024-07-02 12:03:24 +0530

    ...

--- src/Store/PostsStore.jsx
+++ src/Store/PostsStore.jsx
@@ -34,7 +34,7 @@ transition:Bounce
 });
     }
     const notifyWarn=()=>{
-        toast.error('enter mandetory fields', {
+        toast.error('enter mandatory fields', {
 position: "top-center",
 autoClose: 5000,
 hideProgressBar: false,
